acdad784e0ad73d5260fbd5a7f16c13392ee3c53
pbishal <pbishal60@gmail.com>
2021-03-30 09:27:39 +0530

    added program 1

---
+++ BasicProgramsDemo/BasicProgramsDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicProgramsDemo
+{
+    /// <summary>
+    /// Reading input from thr user and replacing user name with template
+    /// </summary>
+    class ReplaceString
+    {
+        public static void Readinput()
+        {
+            string template = "Hello <<username>>, How are you";
+            Console.WriteLine("Please enter your name: ");
+            string userInput = Console.ReadLine();
+            ReplaceWord(template, userInput);
+
+        }
+
+        private static void ReplaceWord(string template, string userInput)
+        {
+            string result = template.Replace("<<UserName>>", userInput);
+            Console.WriteLine("After done replace\t" + result);
+        }
+
+    }
+}
